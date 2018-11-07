232a9ef44fa1cf4220754c698705dbc98d815f8a
Emily Lu <emilylu0819@gmail.com>
2018-11-06 22:51:04 -0500

    update

--- js/header.js
+++ js/header.js
@@ -5,7 +5,7 @@ const Header = () => (
   <header>
     <nav className="navbar navbar-expand-lg bg-secondary text-uppercase" id="mainNav">
     <div className="container">
-    <h1 className="color--white">Emily Lu</h1>
+    <h1 className="color--white">Portfolio</h1>
     <div className="menufont">
     <NavLink to="/" activeClassName="mx-lg-1" exact={true}>Work </NavLink>
     <NavLink to="/about" activeClassName="mx-lg-1">About </NavLink>
